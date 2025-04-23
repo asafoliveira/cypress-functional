02920952e9cd263abc4b5f8ee8f04e78935cf551
Asaf Oliveira <asaf.oliveira.ds@gmail.com>
2025-04-23 20:44:59 -0300

    Created account update scenario

--- cypress/e2e/functional.cy.js
+++ cypress/e2e/functional.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Should test at functional level', () => {
-    before(() => {
+    beforeEach(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
         cy.get('[data-test="email"]').type('papa@bento')
         cy.get('[data-test="passwd"]').type('papabento16')
@@ -15,6 +15,17 @@ describe('Should test at functional level', () => {
         cy.get('[data-test="nome"]').type('Conta Teste')
         cy.get('.btn').click()
         cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
-
     })
+
+    it('Should update an account', () => {
+        cy.get('[data-test="menu-settings"] > .fas').click()
+        cy.get('[href="/contas"]').click()
+        cy.xpath("//table//td[contains(., 'Conta Teste')]/..//i[@class='far fa-edit']")
+        cy.get('[data-test="nome"]').type('Conta Teste')
+          .clear()
+          .type('Conta Alterada2')
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('contain', 'Conta alterada com sucesso')
+    })
+
 })
