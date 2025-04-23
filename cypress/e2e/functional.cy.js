f78ee3ddbd316265f4d04aff5a8747c1e1fbff40
Asaf Oliveira <asaf.oliveira.ds@gmail.com>
2025-04-23 19:45:54 -0300

    Inserted login routine at before block

--- cypress/e2e/functional.cy.js
+++ cypress/e2e/functional.cy.js
@@ -2,13 +2,14 @@
 
 describe('Should test at functional level', () => {
     before(() => {
-        cy.visit('https://barrigareact.wcaquino.me/');
-    })
-
-    it('Login', () => {
+        cy.visit('https://barrigareact.wcaquino.me/')
         cy.get('[data-test="email"]').type('papa@bento')
         cy.get('[data-test="passwd"]').type('papabento16')
         cy.get('.btn').click()
-        cy.get('.toast-message').should('exist')
+        cy.get('.toast-message').should('contain', 'Bem vindo')
+    })
+
+    it('...', () => {
+        
     })
 })
