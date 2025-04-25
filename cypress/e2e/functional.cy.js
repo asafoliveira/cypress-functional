a6678a6a144209116cdb5326fbb9ee0a8bf04da2
Asaf Oliveira <asaf.oliveira.ds@gmail.com>
2025-04-25 18:48:08 -0300

    Created negative account creation scenario

--- cypress/e2e/functional.cy.js
+++ cypress/e2e/functional.cy.js
@@ -6,24 +6,30 @@ import '../support/commandsContas'
 describe('Should test at functional level', () => {
     beforeEach(() => {
         cy.login('papa@bento', 'papabento16')
-        cy.resetApp()
     })
 
     it('Should create an account', () => {
+        cy.resetApp()
         cy.acessarMenuConta()
-        cy.inserirConta()
+        cy.inserirConta('Conta Teste')
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     })
 
     it('Should update an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
+        cy.acessarMenuConta()
         cy.xpath(loc.CONTAS.XP_BTN_ALTERAR)
         cy.get(loc.CONTAS.NOME).type('Conta Teste')
           .clear()
-          .type('Conta Alterada2')
+          .type('Conta Alterada')
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'Conta alterada com sucesso')
+    })
+
+    it('Should not create an account with same name', () => {
+        cy.acessarMenuConta()
+        cy.get(loc.CONTAS.NOME).type('Conta Alterada')
+        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'code 400')
     })
 
 })
