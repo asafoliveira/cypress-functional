15b14a4d08f5c00ea996a2c1dfcc53fdb1ed1544
Asaf Oliveira <asaf.oliveira.ds@gmail.com>
2025-04-24 21:08:03 -0300

    added commands file for 'Contas' routine

--- cypress/e2e/functional.cy.js
+++ cypress/e2e/functional.cy.js
@@ -1,17 +1,17 @@
 /// <reference types="cypress" />
 
 import loc from '../support/locators'
+import '../support/commandsContas'
 
 describe('Should test at functional level', () => {
     beforeEach(() => {
         cy.login('papa@bento', 'papabento16')
+        cy.resetApp()
     })
 
     it('Should create an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Conta Teste')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.acessarMenuConta()
+        cy.inserirConta()
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,3 +7,8 @@ Cypress.Commands.add('login', (user, pwd) => {
     cy.get(loc.LOGIN.BTN_LOGIN).click()
     cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
 })
+
+Cypress.Commands.add('resetApp', () => {
+    cy.get(loc.MENU.SETTINGS).click()
+    cy.get(loc.MENU.RESET).click()
+})
